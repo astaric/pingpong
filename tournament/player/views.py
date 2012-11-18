24126ffb74fada7142d1a54da0687b0476797007
astaric <anze.staric@gmail.com>
2012-11-18 01:04:02 +0100

    List players by categories.

--- tournament/player/views.py
+++ tournament/player/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render_to_response, render
 from . import models
 
 def list(request):
-    players = models.Player.objects.all()
-    return render(request, "list.html", {"players": players})
+    categories = models.Category.objects.all()
+    players = models.Player.objects.all().order_by('category')
+    players_by_categories = [
+        (category, players.filter(category=category))
+        for category in categories
+    ]
+    print players_by_categories
+    return render(request, "list.html", {"categories": players_by_categories})
 
 def show(request, id):
     player = models.Player.objects.get(id=id)
