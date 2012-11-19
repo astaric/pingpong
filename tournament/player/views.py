f4bb808296b44147143fede122ae70421dacd352
astaric <anze.staric@gmail.com>
2012-11-19 20:16:59 +0100

    Added a sample match card.

--- tournament/player/views.py
+++ tournament/player/views.py
@@ -1,17 +1,36 @@
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
 
 from . import models
 
-def list(request):
+def index(request):
     categories = models.Category.objects.all()
     players_by_categories = [
         (category, category.player_set.order_by('surname', 'name'))
         for category in categories
     ]
-    print players_by_categories
     return render(request, "list.html", {"categories": players_by_categories})
 
-def show(request, id):
+def details(request, id):
     player = models.Player.objects.get(id=id)
     return render_to_response("show.html", {"player": player})
 
+
+match_template = ('','','','','','','','','','','','','','','','','','','',
+'+-----------------------------------------------------------------+----------+',
+'|                                                                 | Miza     |',
+'+-----------------------------------------------------------------+----------+',
+'|                                                                            |',
+'+----------+----------+----------+----------+----------+----------+----------+',
+'|          |          |          |          |          | Skupaj:  |          |',
+'+----------+----------+----------+----------+----------+----------+----------+',
+)
+
+def print_match(request, id=0):
+    template = map(list, match_template)
+    template[20][2:66] = "Anze Staric".ljust(64)
+    template[22][2:66] = "Filip Evgen Trdan Staric".ljust(64)
+    template[20][-4:-2] = "15".rjust(2)
+    template = map(lambda x: u"".join(x), template)
+
+    return HttpResponse(u"\n".join(template), content_type="text/plain; charset=utf-8")
