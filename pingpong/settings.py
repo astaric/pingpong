8e808b7d35aaaffcd797b50b79517b41e1fefa2b
astaric <anze.staric@gmail.com>
2013-12-27 09:51:27 +0100

    Use project based path for printing.

--- pingpong/settings.py
+++ pingpong/settings.py
@@ -99,7 +99,7 @@ STATIC_URL = '/static/'
 
 # Project settings
 
-PRINT_DIRECTORY = "/Users/anze/dev/pingpong_produkcija/print/"
+PRINT_DIRECTORY = os.path.join(BASE_DIR, "..", "print")
 
 # Heroku settings
 # Parse database configuration from $DATABASE_URL
