1ecc7bc1adac2e1d057bc2e45d6d8aba412a6b18
astaric <anze.staric@gmail.com>
2013-12-31 10:19:54 +0100

    Refactor category templates.

--- pingpong/views/__init__.py
+++ pingpong/views/__init__.py
@@ -1,13 +1,18 @@
 from django.core.urlresolvers import reverse
+from django.db.models import Count
 from django.shortcuts import redirect
 from pingpong.models import Category
 
 
 def index(request):
     try:
-        category = Category.objects.all()[0]
-        return redirect(reverse('category_edit', kwargs=dict(category_id=category.id)))
-    except IndexError:
-        pass
+        category = Category.objects.annotate(Count('bracket'), Count('group'))[0:1].get()
 
-    return redirect(reverse('category_add'))
+        if category.group__count > 0:
+            pass
+        elif category.bracket__count > 0:
+            pass
+        else:
+            return redirect(reverse('category_edit', kwargs=dict(category_id=category.id)))
+    except Category.DoesNotExist:
+        return redirect(reverse('category_add'))
