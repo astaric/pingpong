9a89dff9b0eb90bf4048cef9d677ab7e564a0193
astaric <anze.staric@gmail.com>
2014-01-24 09:52:52 +0100

    Extract touch firendly number input widget.

---
+++ pingpong/static/js/touch-number.js
@@ -0,0 +1,26 @@
+$.fn.create_touch_targets = function (options) {
+    this.each(function () {
+        console.log(options);
+        options = options || {};
+        console.log(options);
+        var $this = $(this);
+        var name = this.name;
+        var value = this.value;
+
+        var touch_targets = '';
+        for (var val = options.min_value; val <= options.max_value; val++) {
+            var id = name + '-' + val;
+            var selected = (value == val) ? ' checked' : '';
+            if (value) {
+                $this.parent().addClass('selected');
+            }
+
+            touch_targets +=
+                '<input type="radio" id="' + id + '" name="' + name + '" value="' + val + '"' + selected + '>' +
+                    '<label for="' + id + '">' + val + '</label>';
+        }
+
+        $this.replaceWith(touch_targets);
+    });
+    return this
+};
